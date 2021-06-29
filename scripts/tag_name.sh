c6b8ee0668f9cd402aee0c49b0fc449da3c0ab46
Romain Lecomte <romain@conduktor.io>
2021-06-29 17:19:38 +0200

    wip

--- scripts/tag_name.sh
+++ scripts/tag_name.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
 RELEASE_TYPE=$1
+VERSION=$2
+NUM=$3
 
-if [ "$RELEASE_TYPE" = "staging" ]; then
-  "$VERSION-RC"
-if [ "$RELEASE_TYPE" = "prod" ]; then
-  "$VERSION"
-else
+if [[ ! $VERSION =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+  echo "Something is wrong with your version. (eg: 1.2.3)"
   exit 1
 fi
+
+if [[ ! NUM =~ ^([0-9]+)$ ]]; then
+  echo "Something is wrong with release candidate number."
+  exit 1
+fi
+
+if [[ "$RELEASE_TYPE" = "staging" ]]; then
+  echo "$VERSION-rc.$NUM"
+elif [[ "$RELEASE_TYPE" = "prod" ]]; then
+  echo "$VERSION"
+else
+  echo "release type should be [staging|prod]"
+  exit 1
+fi
+
